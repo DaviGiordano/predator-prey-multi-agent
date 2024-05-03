6e979fcb0f197cce7ef24ff23a7500bc37fe71bf
DaviGiordano <davigiordano2704@gmail.com>
2024-05-03 13:47:52 +0100

    Chore: Adjust Imports

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from modules.simulation import Simulation
+if __name__ == "__main__":    
+    N_SIMS = 10
+    i = 0
+    s = Simulation(strategy='random')
+    while i < N_SIMS:
+        print(f"Simulation {i}/{N_SIMS}")
+        print(s.run())
+        s.reset()
+        i += 1
 
-N_SIMS = 10
-i = 0
-s = Simulation(strategy='random')
-while i < N_SIMS:
-    print(f"Simulation {i}/{N_SIMS}")
-    print(s.run())
-    s.reset()
-    i += 1
 
