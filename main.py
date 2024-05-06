34932311da657ce2e6ca377486fb5835a32eace7
DaviGiordano <davigiordano2704@gmail.com>
2024-05-06 21:30:14 +0100

    Chore: Adjust print

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from modules.environment.simulation import Simulation
 if __name__ == "__main__":    
-    N_SIMS = 10
+    N_SIMS = 1
     i = 0
-    s = Simulation(strategy='random', render='human')
+    s = Simulation(strategy='greedy', render='human')
     while i < N_SIMS:
         print(f"Simulation {i}/{N_SIMS}")
-        print(s.run())
+        first_catch, num_catches = s.run()
+        print(f'first_catch:{first_catch}\nnum_catches: {num_catches}')
         s.reset()
         i += 1
 
