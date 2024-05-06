c5d380bfb38a00ebd0697d05fb8267d0b0f5e865
DaviGiordano <davigiordano2704@gmail.com>
2024-05-06 21:30:03 +0100

    Feat: Build Greedy adversary

--- modules/actors/adversaries/greedy_adversary.py
+++ modules/actors/adversaries/greedy_adversary.py
@@ -1,5 +1,5 @@
 from modules.actors.adversaries import BaseAdversary
-
+from modules.actors.constants import action_codes as act
 class GreedyAdversary(BaseAdversary):
     ''' ... '''
     def __init__(self, id, initial_observation):
@@ -8,5 +8,20 @@ class GreedyAdversary(BaseAdversary):
     def get_action(self):
         # Calculate distance from prey using last observations
         # Choose action of direction that minimizes the distance
-        raise NotImplementedError
 
+        dx = self.last_observation['ag_relpos_x']
+        dy = self.last_observation['ag_relpos_y']
+        action = act['no_action']
+
+        if abs(dx) > abs(dy): # Horizontal distance is bigger
+            if dx < 0:  # Prey is to the left
+                action = act['move_left']
+            elif dx > 0:  # Prey is to the right
+                action = act['move_right']
+        else:
+            if dy < 0:  # Prey is below
+                action = act['move_down']
+            elif dy > 0:  # Prey is above
+                action = act['move_up']
+        
+        return action
