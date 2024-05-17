a95ee3fcf1caaaf8fc3a63545b31818806a981d4
DaviGiordano <davigiordano2704@gmail.com>
2024-05-17 12:39:49 +0100

    Define window_size pygame

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from modules.environment.simulation import Simulation
+import pygame
+import os
+# Set the window position before initializing Pygame
+x, y = 50, 100   # Set the desired window position
+os.environ['SDL_VIDEO_WINDOW_POS'] = f"{x},{y}"
 if __name__ == "__main__":    
     N_SIMS = 1
     i = 0
-    s = Simulation(strategy='surround-greedy', render='human')
+    s = Simulation(strategy='greedy-bounded', render='human')
+    pygame.display.set_mode((100, 100))
     while i < N_SIMS:
         print(f"Simulation {i}/{N_SIMS}")
         first_catch, num_catches = s.run()
